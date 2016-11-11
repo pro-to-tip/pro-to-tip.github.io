ca650620f26d8cb49440e2a2ba2b17f28324206c
pro-to-tip <nenjoi@yahoo.com>
2016-11-11 17:08:39 +0300

    Create deliveryCalculator.js

---
+++ ajax/scss/deliveryCalculator.js
@@ -0,0 +1,67 @@
+ymaps.ready(['DeliveryCalculator']).then(function init () {
+    var myMap = new ymaps.Map('map', {
+            center: [60.906882, 30.067233],
+            zoom: 9,
+            type: 'yandex#map',
+            controls: []
+        }),
+        searchStartPoint = new ymaps.control.SearchControl({
+            options: {
+                useMapBounds: true,
+                noPlacemark: true,
+                noPopup: true,
+                placeholderContent: 'Адрес начальной точки',
+                size: 'large'
+            }
+        }),
+        searchFinishPoint = new ymaps.control.SearchControl({
+            options: {
+                useMapBounds: true,
+                noCentering: true,
+                noPopup: true,
+                noPlacemark: true,
+                placeholderContent: 'Адрес конечной точки',
+                size: 'large',
+                float: 'none',
+                position: { left: 10, top: 44 }
+            }
+        }),
+        calculator = new ymaps.DeliveryCalculator(myMap);
+
+    myMap.controls.add(searchStartPoint);
+    myMap.controls.add(searchFinishPoint);
+
+    searchStartPoint.events
+        .add('resultselect', function (e) {
+            var results = searchStartPoint.getResultsArray(),
+                selected = e.get('index'),
+                point = results[selected].geometry.getCoordinates();
+
+            // Задаем начало маршрута.
+            calculator.setStartPoint(point);
+        })
+        .add('load', function (event) {
+            // По полю skip определяем, что это не дозагрузка данных.
+            // По getResultsCount определяем, что есть хотя бы 1 результат.
+            if (!event.get('skip') && searchStartPoint.getResultsCount()) {
+                searchStartPoint.showResult(0);
+            }
+        });
+
+    searchFinishPoint.events
+        .add('resultselect', function (e) {
+            var results = searchFinishPoint.getResultsArray(),
+                selected = e.get('index'),
+                point = results[selected].geometry.getCoordinates();
+
+            // Задаем конец маршрута.
+            calculator.setFinishPoint(point);
+        })
+        .add('load', function (event) {
+            // По полю skip определяем, что это не дозагрузка данных.
+            // По getResultsCount определяем, что есть хотя бы 1 результат.
+            if (!event.get('skip') && searchFinishPoint.getResultsCount()) {
+                searchFinishPoint.showResult(0);
+            }
+        });
+});
