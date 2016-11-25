074c3776708cdd17a973cd6e3a3431660a54f478
pro-to-tip <nenjoi@yahoo.com>
2016-11-25 14:43:23 +0300

    Update clock.js

--- home-portal-app/js/clock.js
+++ home-portal-app/js/clock.js
@@ -45,7 +45,7 @@
   for (var i = 0; i < screen.width + 5; i += 5) {
     for (var j = 0; j < screen.height + 5; j += 5) {
       ict.beginPath();
-      ict.arc(i, j, 2, 0, 2 * Math.PI);
+      ict.arc(i, j, 3, 0, 3 * Math.PI);
       ict.fillStyle = '#fafbfc';
       ict.fill();
       ict.closePath();
